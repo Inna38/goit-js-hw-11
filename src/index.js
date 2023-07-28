036ab8c573d2d1687f0f46d5ce68efa936069e7b
Inna Didenko <innadidenko29@gmail.com>
2023-07-28 23:23:42 +0300

    update task

--- src/index.js
+++ src/index.js
@@ -6,12 +6,18 @@ const BASE__URL = "https://pixabay.com/api/"
 
 const searchForm = document.querySelector(".search-form")
 const gallery = document.querySelector(".gallery")
+const loadMore = document.querySelector(".load-more")
+let page = 1
 
 searchForm.addEventListener("submit", onsearchFormSubmit)
+loadMore.addEventListener("click", onLoadMoreClick)
 
-async function getUser() {
+
+
+async function getUser(searchEl, page) {
+
 // return response = await axios.get(`${BASE__URL}?key=${API__KEY}&q=cat&image_type=photo&orientation=horizontal&safesearch=true`);
-        const response = await fetch(`${BASE__URL}?key=${API__KEY}&q=cat&image_type=photo&orientation=horizontal&safesearch=true`);
+        const response = await fetch(`${BASE__URL}?key=${API__KEY}&q=${searchEl}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`);
     if (!response.ok) {
     throw new Error(response.statusText);
     }
@@ -21,38 +27,85 @@ async function getUser() {
 
 
 function onsearchFormSubmit(e) {
-    e.preventDefault()
-    const searchEl = e.currentTarget.elements.searchQuery.value
-    getUser().then(resp => gallery.insertAdjacentHTML("beforeend",
-    markup(resp.hits))).catch(err => console.log(err));
+  e.preventDefault()
+  const searchEl = e.currentTarget.elements.searchQuery.value
+  localStorage.setItem("keyEl", searchEl)
+  
+ getUser(searchEl,1).then(resp => gallery.insertAdjacentHTML("beforeend", markup(resp.hits))).catch(err => console.log(err));
+  // e.currentTarget.reset()
+gallery.innerHTML = ""
+page = 1
 };
 
-   
+
+
 function markup(arr) {
-  console.log(arr)
-
-  return arr.map(({ likes, views, comments, downloads }) =>
-
-        `<div class="photo-card">
-  <img src="" alt="" loading="lazy" />
+  return arr.map(({ largeImageURL, webformatURL, likes, views, comments, downloads }) =>
+  `<div class="photo-card">
+  <img src="${webformatURL}" alt="" loading="lazy" />
   <div class="info">
     <p class="info-item">
-      <b>${likes}</b>
+      <b>likes: ${likes}</b>
     </p>
     <p class="info-item">
-      <b>${views}</b>
+      <b>views: ${views}</b>
     </p>
     <p class="info-item">
-      <b>${comments}</b>
+      <b>comments: ${comments}</b>
     </p>
     <p class="info-item">
-      <b>${downloads}</b>
+      <b>downloads: ${downloads}</b>
     </p>
   </div>
 </div>`
-    ).join("");
+).join("");
     
 }
+
+function onLoadMoreClick() {
+  const local = localStorage.getItem("keyEl")
+  page += 1
+  getUser(local,page).then(resp => gallery.insertAdjacentHTML("beforeend", markup(resp.hits))).catch(err => console.log(err));
+}
+
+
+
+
+// class ApiService {
+//   constructor() {
+//     this.searchQuery = "";
+//      this.page = 1;
+//   }
+
+// async  getUser() {
+//  page += 1
+// // return response = await axios.get(`${BASE__URL}?key=${API__KEY}&q=cat&image_type=photo&orientation=horizontal&safesearch=true`);
+//         const response = await fetch(`${BASE__URL}?key=${API__KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=4`);
+//     if (!response.ok) {
+//     throw new Error(response.statusText);
+//     }
+//     return response.json()
+// };
+
+
+
+//   get query() {
+//   return this.searchQuery
+//   }
+//   set query(newQuery) {
+//     this.searchQuery = newQuery
+//   }
+
+// }
+
+
+
+
+
+
+
+
+
 // webformatURL,largeImageURL,tags,
 
 // ({hits: {likes,views,comments,downloads}})
